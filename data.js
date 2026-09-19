f1dee6675952ec37c62bb3265e0e962002fdbe49
Nikubai12 <68363007+Nikubai12@users.noreply.github.com>
2026-09-19 11:42:53 -0700

    Update data.js

--- data.js
+++ data.js
@@ -211,7 +211,7 @@ const PIE_CATEGORIES = [
         raised: 300,
         donors: [
           { name: "Darren Thompson", amount: 100 },
-          {name: "Alan Chee", amount: 50},
+          {name: "Alan Chee", amount: 100},
            {name: "Ashlee Fuller", amount: 50},
         ]
       }
